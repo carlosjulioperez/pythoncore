7e4bde89e0edb8a9c96b8ad5aa933281f18bbd7b
Carlos <carlos.perez2@zionsbancorp.com>
2025-03-18 22:53:09 +0000

    Strings

--- strings.py
+++ strings.py
@@ -2,4 +2,27 @@ sentence = "the dog is named Sammy"
 print(sentence.upper())
 print(sentence.lower())
 print(sentence.capitalize())
-print(sentence.count('a'))
+print(sentence.count('a')) 
+
+# Slicing
+Letters="ABCDEFGHIJK"
+print(Letters[0:4]) # ABCD
+
+# Use a stride value of 2 on the following string :
+Good="GsoAo+d"
+print(Good[::2]) # Good
+
+# Uppercase
+print("uppercase".upper())
+
+name ="Michael Jackson" 
+print (len(name))
+
+# Take the slice on variable name with only index 0 to index 3
+print (name[0:4]) # Mich
+
+# Take the slice on variable name with only index 8 to index 11
+print (name[8:12]) # Jack
+
+# Get every second element. The elments on index 1, 3, 5 ...
+print (name[::2]) # McalJcsn
