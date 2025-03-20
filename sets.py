4e3bda7b6c50e643f00a5d40a6cbb3baa33b47bf
Carlos <carlos.perez2@zionsbancorp.com>
2025-03-20 23:09:57 +0000

    sets examples

---
+++ sets.py
@@ -0,0 +1,14 @@
+# Set = Conjunto
+
+# Cast the following list to a set:
+print(['A','B','C','A','B','C'])
+print(set(['A','B','C','A','B','C']))
+# ['A', 'B', 'C', 'A', 'B', 'C']
+# Set hasn't permit duplicates
+# {'C', 'B', 'A'}
+
+# Add the string 'D' to the set S.
+S={'A','B','C'}
+S.add('D')
+print(S)
+# {'D', 'B', 'C', 'A'}
